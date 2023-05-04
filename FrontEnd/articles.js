30ddbcedd8303dc7ae0c0c66fdaf35ae30e4cf05
Caroline Sclavon <caroline.sclavon@gmail.com>
2023-05-04 17:25:47 +0200

    Portfolio-architecte-sophie-bluel

---
+++ FrontEnd/articles.js
@@ -0,0 +1,11 @@
+class article {
+    constructor(jsonArticle){
+        jsonArticle && Object.assign(this, jsonArticle);
+    }
+
+    getFormatedDate(article){
+        let timestamp = Date.parse(this.publicationDate);
+        let date = new Date(timestamp);
+        return date.toLocaleTimeString()
+    }
+}
