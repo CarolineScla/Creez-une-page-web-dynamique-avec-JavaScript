b4bc06be1beb1ff253a421284ad4ce9f6996deaf
Caroline Sclavon <caroline.sclavon@gmail.com>
2023-05-18 12:26:31 +0200

    P3

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,41 +1,24 @@
 /** Gère l'affichage de la page d'accueil **/
-
-"use strict"; // pour limiter les erreurs dans le code 
-
-/**
 fetch("http://localhost:5678/api/works")
-  .then(response => {
-    if (response.ok) return response.json();
-  })
-  .then((data) => { /* affichage des projets avec le create element sur le dom
-    listProjet = data;
-    displayProjects(listProjet);
-    displayEdit();
-
-    data.forEach((work) => {
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((element) => {
+      console.log(element);
+      
       const figure = document.createElement("figure");
       const img = document.createElement("img");
       const figcaption = document.createElement("figcaption");
 
-      img.src = work.imageUrl;
-      img.alt = work.title;
-      figcaption.textContent = work.title;
+      img.src = element.imageUrl;
+      img.alt = element.title;
+      figcaption.textContent = element.title;
 
       figure.appendChild(img);
       figure.appendChild(figcaption);
-
-      gallery.appendChild(figure);
+      portfolio.appendChild(figure);
     });
   })
-  .catch((error) => {
-    alert("Une erreur est survenue! Veuillez contacter l'administrateur!");
-  }); */ 
+  .catch((error) => console.error(error));
 
-  fetch("http://localhost:5678/api-docs/#/")
-  .then( data => data.json())
-  .then( jsonListProjets => {
-    for(le jsonProjets orof jsonListProjets){
-      let projets = new 
-    )
-  });
+
 
